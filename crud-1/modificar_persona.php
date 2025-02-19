42e8c6112e427b9e14a5de93ed3657d253f9ec09
Fabian Mella Barra <fa.mella@duocuc.cl>
2025-02-19 12:41:00 -0300

    Update modificar_persona.php

--- crud-1/modificar_persona.php
+++ crud-1/modificar_persona.php
@@ -18,38 +18,42 @@ $sql = $conexion->query("select * from persona where id=$id");
 </head>
 
 <body>
-    <h1 class="text-center m-5">Modificación de usuario</h1>
-    <form class="col-4 p-3 m-auto" method="POST">
-        <input type="text" name="id" value="<?= $GET_["id"] ?>" hidden>
-        <?php
-        include "controlador/modificar_persona.php";
-        while ($datos = $sql->fetch_object()) { ?>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Nombre</label>
-                <input type="text" class="form-control" name="nombre" value="<?= $datos->nombre?>">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Apellido</label>
-                <input type="text" class="form-control" name="apellido" value="<?= $datos->apellido?>">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">DNI</label>
-                <input type="text" class="form-control" name="dni" value="<?= $datos->dni?>">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Fecha de nacimiento</label>
-                <input type="date" class="form-control" name="fech_nacimiento" value="<?= $datos->fech_nacimiento?>">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Correo</label>
-                <input type="email" class="form-control" name="correo" value="<?= $datos->correo?>">
-            </div>
-        <?php }
-        ?>
+    <div class="col-8 p-3 m-auto">
+        <a href="lista_personas.php"><i class="fa-solid fa-arrow-left"></i></a>
+        <h1 class="text-center m-5">Modificación de usuario</h1>
+        <form class="col-6 p-3 m-auto" method="POST">
+            <input type="text" name="id" value="<?= $GET_["id"] ?>" hidden>
+            <?php
+            include "controlador/modificar_persona.php";
+            while ($datos = $sql->fetch_object()) { ?>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Nombre</label>
+                    <input type="text" class="form-control" name="nombre" value="<?= $datos->nombre ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Apellido</label>
+                    <input type="text" class="form-control" name="apellido" value="<?= $datos->apellido ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">DNI</label>
+                    <input type="text" class="form-control" name="dni" value="<?= $datos->dni ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Fecha de nacimiento</label>
+                    <input type="date" class="form-control" name="fech_nacimiento" value="<?= $datos->fech_nacimiento ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Correo</label>
+                    <input type="email" class="form-control" name="correo" value="<?= $datos->correo ?>">
+                </div>
+            <?php }
+            ?>
 
 
-        <button type="submit" class="btn btn-primary" name="modificar" value="ok">Modificar datos de usuario</button>
-    </form>
+            <button type="submit" class="btn btn-primary" name="modificar" value="ok">Modificar datos de usuario</button>
+        </form>
+    </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
