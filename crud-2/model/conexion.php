8751ff5de5223d41e8fd79a528dbcc7a75dd158f
Fabian Mella Barra <fa.mella@duocuc.cl>
2025-02-20 13:03:35 -0300

    Update conexion.php

--- crud-2/model/conexion.php
+++ crud-2/model/conexion.php
@@ -1,8 +1,4 @@
 <?php
-    $conexion = new mysqli('localhost', 'root', '', 'crud_php');
-    if($conexion->connect_error){
-        echo "Error en la conexion";
-    }else{
-        echo "Conexion exitosa";
-    }
+$conexion = new mysqli("localhost", "root", "", "crud_php");
+$conexion->set_charset("utf8");
 ?>
