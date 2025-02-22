a8e4b7d0c9c3d42e2b2c3fc712ef62c86f1a3890
Fabian Mella Barra <fa.mella@duocuc.cl>
2025-02-22 19:51:42 -0300

    Update conexion.php

--- crud-2/model/conexion.php
+++ crud-2/model/conexion.php
@@ -1,4 +1,10 @@
 <?php
 $conexion = new mysqli("localhost", "root", "", "crud_php");
 $conexion->set_charset("utf8");
+
+function ejecutarQuery($query){
+    global $conexion;
+    $resultado = $conexion->query($query);
+    return $resultado;
+}
 ?>
